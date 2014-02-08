ac348944f7bdd93f5080b7b47711c4d30c4241c0
Joshua Ellington <hello@joshellington.com>
2014-02-08 12:37:13 -0800

    Working with config.yml file, only supports Tumblr

---
+++ cronwall.rb
@@ -0,0 +1,52 @@
+require 'yaml'
+require 'open-uri'
+require 'uri'
+require 'json'
+require 'ap'
+
+CONFIG = YAML.load_file('config.yml')
+
+def fetch(site)
+  if site['type'] == 'tumblr'
+    host = URI.parse(site['url']).host
+    api_url = "http://api.tumblr.com/v2/blog/#{host}/posts?api_key=#{CONFIG['api_keys']['tumblr']}&notes_info=false"
+    posts = JSON.parse(open(api_url).read)['response']['posts']
+
+    posts.each do |post|
+      if post['photos']
+        photo = post['photos'][0]['original_size']
+        
+        if photo['width'] > 1100
+          save(photo['url'])
+        end
+      end
+    end
+  else
+    ap 'Sorry, your site type is not supported.'
+  end
+end
+
+def save(url)
+  filename = File.basename(URI.parse(url).path)
+  save_path = CONFIG["path"] + filename
+
+  unless File.exists? save_path
+    image = open(url).read
+    File.open(save_path, 'w') { |file| file.write(image) }
+    ap "Success: #{filename}"
+  else
+    ap "Error: #{filename} already exists."
+  end
+end
+
+def init
+  if File.exists? CONFIG["path"]
+    CONFIG["sites"].each do |site|
+      fetch(site)
+    end
+  else
+    ap "Your path does not exist. Try again."
+  end
+end
+
+init()
